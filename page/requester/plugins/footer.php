181ca79dae1fe5e54166d3f07f13f6c65cb9ed33
prince-i <prince_arce01@outlook.com>
2022-09-14 18:41:25 +0800

    UPDATE

--- page/requester/plugins/footer.php
+++ page/requester/plugins/footer.php
@@ -1,5 +1,5 @@
  <footer class="main-footer">
-    <strong>Copyright &copy; 2022. Developed by: JJ Buendia</strong>
+    <strong>Copyright &copy; 2022.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0
